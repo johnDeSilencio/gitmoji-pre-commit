a2b734eef26475da7f5582d35c4273df55251b9b
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2024-08-13 22:02:27 -0700

    Read and write from COMMIT_EDITMSG directly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use conventional::CommitMessage;
-use std::env::args;
+use std::fs;
+use std::path::Path;
 
 mod conventional {
     use regex::Regex;
@@ -74,19 +75,28 @@ mod conventional {
 }
 
 fn main() {
-    let args = args();
+    if !Path::new("./.git/").try_exists().unwrap() {
+        panic!("must run this hook in a git repository");
+    }
 
-    println!("{:?}", args);
+    if !Path::new("./.git/COMMIT_EDITMSG").try_exists().unwrap() {
+        panic!("must run this hook in the commit-msg stage");
+    }
 
-    let Some(commit_msg) = args.into_iter().nth(1) else {
-        panic!("expected commit message as a command-line argument!");
-    };
+    let commit_msg = fs::read("./.git/COMMIT_EDITMSG").expect("we can read from the file");
 
     // Source: https://github.com/folke/devmoji?tab=readme-ov-file#default-devmoji-reference
-    let conventional_commit_msg = CommitMessage::try_new(commit_msg).unwrap();
+    let conventional_commit_msg = CommitMessage::try_new(
+        String::from_utf8(commit_msg).expect("no invalid UTF-8 in commit message"),
+    )
+    .unwrap();
     let modified_commit_msg = conventional_commit_msg.insert_emoji().unwrap();
 
-    println!("{}", modified_commit_msg);
+    fs::write(
+        "./.git/COMMIT_EDITMSG",
+        modified_commit_msg.to_string().as_bytes(),
+    )
+    .expect("we can write to the file");
 }
 
 #[cfg(test)]
